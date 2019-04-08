f03b12c4261a0bc101888b2ad2dd9d10853e6ca0
lupini-beans <lupi.em95@gmail.com>
2019-04-08 15:41:56 -0600

    first attempt at db schema

--- app/models.py
+++ app/models.py
@@ -12,9 +12,61 @@ class User(db.Model, UserMixin):
     # Formerly username
     email = db.Column(db.String(256), unique=True) 
     f_name = db.Column(db.String(30))
+    username = db.Column(db.String(256))
+    profile_photo = db.Column(db.LargeBinary)
+    algo = db.Column(db.LargeBinary)
+
+    books = relationship(User_Books, back_populates="user")
 
     def __repr__(self):
         return '<User {}>'.format(self.email)
+
+class User_Books(db.Model):
+    # Concatenation of volume_id and a users id
+    volume_user = db.Column(db.String(256), primary_key=True)
+    volume = db.Column(db.String(256))
+
+    user_rating = db.Column(db.Integer)
+    user_pred = db.Column(db.Float)
+
+    user = relationship(User, back_populates="books")
+
+class Club_Books(db.Model):
+     # Concatenation of volume_id and a club id
+     volume_club = db.Column(db.String(256), primary_key=True)
+     volume = db.Column(db.String(256))
+
+     club_pred = db.Column(db.Float)
+
+     club = relationship(Book_Club, back_populates="books")
+
+class Book_Club(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    pres_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    host_id = db.Column(db.Integer, db.ForeignKey(User.id))
+
+    name = db.Column(db.String(256))
+
+    forums = relationship(Forums, back_populates="club")
+    books = relationship(Club_Books, back_populates="club")
+
+class Forums(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(100))
+
+    club = relationship(Book_Club, back_populates="forums")
+    posts = relationship(Posts, back_populates="forum")
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    author = db.Column(db.Integer, db.ForeignKey(user.id))
+    post = db.Column(db.Text)
+    time = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    forum = relationship(Forums, back_populates="posts")
 
 class OAuth(db.Model, OAuthConsumerMixin):
     user_id = db.Column(db.Integer, db.ForeignKey(User.id))
