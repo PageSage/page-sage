a00d861fc35029238620801f0179a738acba81db
lupini-beans <lupi.em95@gmail.com>
2019-04-10 21:13:50 -0600

    working db, excluding many-to-many relationship

--- app/models.py
+++ app/models.py
@@ -7,89 +7,74 @@ from flask_dance.consumer import oauth_authorized, oauth_error
 from flask_login import UserMixin
 import datetime
 
+# club_members = db.Table('club_members', \
+#         db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True), \
+#         db.Column('club_id', db.Integer, db.ForeignKey('bookclub.id'), primary_key=True))
+
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     # Formerly username
     email = db.Column(db.String(256), unique=True) 
     f_name = db.Column(db.String(30))
-    username = db.Column(db.String(256))
-    profile_photo = db.Column(db.LargeBinary)
+    username = db.Column(db.String(256), nullable=True)
+    profile_photo = db.Column(db.LargeBinary, nullable=True)
     algo = db.Column(db.LargeBinary, nullable=True)
 
-    user_books = db.relationship('User_Books', backref=db.backref('book_user'))
-    clubs = db.relationship('Bookclub', secondary='club_members', lazy='subquery', backref=db.backref('club_users'))
+    books = db.relationship('User_Shelf', backref=db.backref('reader', lazy=True))
+    # clubs = db.relationship('Bookclub', secondary=club_members, lazy='subquery', backref=db.backref('members', lazy=True))
 
     def __repr__(self):
         return '<User {}>'.format(self.email)
-                    
-class User_Books(db.Model):
-    __tablename__ = 'user_books'
-    # Concatenation of volume_id and a users id
-    volume_user = db.Column(db.String(256), primary_key=True)
-    volume = db.Column(db.String(256))
 
-    user_rating = db.Column(db.Integer)
+class User_Shelf(db.Model):
+    __tablename__ = 'user_shelf'
+    id = db.Column(db.String(256), primary_key=True)
+    volume_id = db.Column(db.String(256))
+    user_rating = db.Column(db.Integer, nullable=True)
     user_pred = db.Column(db.Float)
-    user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    notes = db.relationship('Notes', backref=db.backref('book_notes'))
+    notes = db.relationship('Notes', backref=db.backref('book_note', lazy=True))
 
 class Bookclub(db.Model):
     __tablename__ = 'bookclub'
     id = db.Column(db.Integer, primary_key=True)
-
-    pres_id = db.Column(db.Integer, db.ForeignKey(User.id))
-    host_id = db.Column(db.Integer, db.ForeignKey(User.id))
-
     name = db.Column(db.String(256))
+    president = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    host = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
 
-    forums = db.relationship('Forums', backref=db.backref('club_forum'), lazy=True)
-    books = db.relationship('Club_Books', backref=db.backref('club_id'), lazy=True)
-    users = db.relationship('User', secondary='club_members', lazy='subquery', backref=db.backref('user_clubs'))
+    books = db.relationship('Club_Shelf', backref=db.backref('club_book', lazy=True))
 
-class Club_Members(db.Model):
-    __tablename__ = 'club_members'
-    user_id = db.Column(db.Integer, db.ForeignKey(User.id), primary_key=True, nullable=True)
-    bookclub_id = db.Column(db.Integer, db.ForeignKey(Bookclub.id), primary_key=True, nullable=True)
-
-class Club_Books(db.Model):
-    __tablename__ = 'club_books'
-     # Concatenation of volume_id and a club id
-    volume_club = db.Column(db.String(256), primary_key=True)
-    volume = db.Column(db.String(256))
-
+class Club_Shelf(db.Model):
+    __tablename__ = 'club_shelf'
+    id = db.Column(db.String(256), primary_key=True)
+    volume_id = db.Column(db.String(256))
     club_pred = db.Column(db.Float)
-    club = db.Column(db.Integer, db.ForeignKey('bookclub.id'), nullable=False)
 
-# Club_Members = db.Table('club_members', \
-#         db.Column('user_id', db.Integer, db.ForeignKey(User.id), primary_key=True), \
-#         db.Column('bookclub_id', db.Integer, db.ForeignKey(Bookclub.id), primary_key=True))
+    club = db.Column(db.Integer, db.ForeignKey('bookclub.id'))
 
 class Forums(db.Model):
     __tablename__ = 'forums'
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256))
 
-    name = db.Column(db.String(100))
-    club = db.Column(db.Integer, db.ForeignKey('bookclub.id'), nullable=False)
+    posts = db.relationship('Forum_Posts', backref=db.backref('forum_post', lazy=True))
 
-    posts = db.relationship('Posts', backref=db.backref('post_forum'))
-
-class Posts(db.Model):
-    __tablename__ = 'posts'
+class Forum_Posts(db.Model):
+    __tablename__ = 'forum_posts'
     id = db.Column(db.Integer, primary_key=True)
-
-    author = db.Column(db.Integer, db.ForeignKey('user.id'))
-    post = db.Column(db.Text)
     time = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    forum = db.Column(db.Integer, db.ForeignKey('forums.id'), nullable=False)
+    content = db.Column(db.Text)
+    author = db.Column(db.Integer, db.ForeignKey('user.id'))
+    forum = db.Column(db.Integer, db.ForeignKey('forums.id'))
 
 class Notes(db.Model):
-    __tablename__ = 'notes'
+    __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
-
-    post = db.Column(db.Text)
-    book = db.Column(db.String(256), db.ForeignKey('user_books.volume_user'), nullable=False)
+    time = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    content = db.Column(db.Text)
+    book = db.Column(db.String(256), db.ForeignKey('user_shelf.id'))
 
 class OAuth(db.Model, OAuthConsumerMixin):
     user_id = db.Column(db.Integer, db.ForeignKey(User.id))
